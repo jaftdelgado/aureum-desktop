399d5291158b12534f3186ad3126828b580a89a3
jaftdelgado <jaftdelgado@gmail.com>
2025-12-01 17:26:56 -0600

    Initial commit: React + Vite setup with Tailwind and hex + vertical slicing project structure

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app"),
+      "@core": path.resolve(__dirname, "src/core"),
+      "@domain": path.resolve(__dirname, "src/domain"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@infra": path.resolve(__dirname, "src/infra"),
+      "@resources": path.resolve(__dirname, "src/resources"),
+    },
+  },
+});
